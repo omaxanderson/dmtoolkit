c63f7f3002352e5d77529ec8ba1379a65cfdbb7b
O Max Anderson <omaxwellanderson@gmail.com>
2018-07-15 22:27:03 +0000

    switched from using the Materialize CDN to local files

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var htmlspecialchars = require('htmlspecialchars');
 
 /* GET requests */
 router.get('/', function(req, res, next) {
